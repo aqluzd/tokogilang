2b12e3a7f625beb1b48a5263e99a84b9c2021c6f
aqluzd <dzulqafauz@gmail.com>
2025-07-12 17:40:39 +0700

    new database

--- tokogilang.sql
+++ tokogilang.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2025 at 03:55 PM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 5.6.19
+-- Generation Time: Jul 12, 2025 at 12:39 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,20 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `permissions` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `permission_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `permissions`
 --
 
-INSERT INTO `permissions` (`id`, `name`, `description`) VALUES
-(1, 'view_dashboard', 'Melihat halaman dashboard'),
-(2, 'manage_products', 'Kelola produk dan stok'),
-(3, 'manage_expenses', 'Kelola biaya operasional'),
-(4, 'manage_employees', 'Kelola data karyawan'),
-(5, 'view_reports', 'Melihat laporan pembukuan');
+INSERT INTO `permissions` (`id`, `permission_name`) VALUES
+(1, 'akses_dashboard'),
+(5, 'edit_pengaturan'),
+(3, 'kelola_karyawan'),
+(2, 'kelola_produk'),
+(4, 'lihat_laporan');
 
 -- --------------------------------------------------------
 
@@ -51,16 +51,15 @@ INSERT INTO `permissions` (`id`, `name`, `description`) VALUES
 
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `roles`
 --
 
-INSERT INTO `roles` (`id`, `name`, `description`) VALUES
-(1, 'owner', 'Pemilik toko dan administrator utama');
+INSERT INTO `roles` (`id`, `role_name`) VALUES
+(1, 'Owner');
 
 -- --------------------------------------------------------
 
@@ -72,18 +71,39 @@ CREATE TABLE `role_permissions` (
   `id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
   `permission_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `role_permissions`
 --
 
 INSERT INTO `role_permissions` (`id`, `role_id`, `permission_id`) VALUES
-(1, 1, 4),
-(2, 1, 3),
-(3, 1, 2),
-(4, 1, 1),
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
 (5, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stores`
+--
+
+INSERT INTO `stores` (`id`, `name`, `address`) VALUES
+(1, 'Hikmah Minimarket', 'Rancah'),
+(2, 'Hikmah Apotek', 'Rancah'),
+(3, 'Hikmah Kosmetik', 'Rancah');
 
 -- --------------------------------------------------------
 
@@ -93,19 +113,19 @@ INSERT INTO `role_permissions` (`id`, `role_id`, `permission_id`) VALUES
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
   `role_id` int(11) NOT NULL,
-  `status` tinyint(1) DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `store_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `name`, `role_id`, `status`) VALUES
-(1, 'owner', '$2y$10$uwlZy3p7EXBtCM.9yGFR.OZELT32manUItYW2tZmCMcZRtPYVqfMW', 'Owner Toko', 1, 1);
+INSERT INTO `users` (`id`, `username`, `password`, `role_id`, `created_at`, `store_id`) VALUES
+(1, 'owner', '$2y$10$eXexK4KKUZ3NADG/HgZ/le6Q2J8QTf/DVK9k6.iDdVghBeWwDn3GC', 1, '2025-07-05 07:41:15', NULL);
 
 --
 -- Indexes for dumped tables
@@ -116,14 +136,14 @@ INSERT INTO `users` (`id`, `username`, `password`, `name`, `role_id`, `status`) 
 --
 ALTER TABLE `permissions`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD UNIQUE KEY `permission_name` (`permission_name`);
 
 --
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD UNIQUE KEY `role_name` (`role_name`);
 
 --
 -- Indexes for table `role_permissions`
@@ -132,6 +152,12 @@ ALTER TABLE `role_permissions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `role_id` (`role_id`),
   ADD KEY `permission_id` (`permission_id`);
+
+--
+-- Indexes for table `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -150,21 +176,31 @@ ALTER TABLE `users`
 --
 ALTER TABLE `permissions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `role_permissions`
 --
 ALTER TABLE `role_permissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- Constraints for dumped tables
 --
@@ -180,7 +216,8 @@ ALTER TABLE `role_permissions`
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
